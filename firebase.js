92e2a90e4577bea67590f61e87fbc89fec566017
chykamran215-collab <chykamran215@gmail.com>
2026-05-31 07:14:03 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnPVVNkzGiCz1j_3PqzP--SQeJWUmGiSU",
+  authDomain: "fast-grocery-3322b.firebaseapp.com",
+  databaseURL: "https://fast-grocery-3322b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fast-grocery-3322b",
+  storageBucket: "fast-grocery-3322b.firebasestorage.app",
+  messagingSenderId: "878961129693",
+  appId: "1:878961129693:web:c06f2dd4fa22ecd4624334",
+  measurementId: "G-16M82PPJTM"
+};
